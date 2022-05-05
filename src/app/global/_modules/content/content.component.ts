fd3e6122c976e4cc51fefd53bd2eee677aacfd42
JuanJesusTorres <juan.torres@urvenue.com>
2022-05-05 17:45:13 -0500

    fixed little issue

--- src/app/global/_modules/content/content.component.ts
+++ src/app/global/_modules/content/content.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  minheight: string;
   constructor() { }
-
+  
   ngOnInit(): void {
+    let height =  window.outerHeight;
+    height = height/2;
+    height += 150;
+    this.minheight = height.toString() + 'px';
+    console.log( this.minheight );
   }
 
 }
